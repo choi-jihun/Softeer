b5de310a70b16868f61e413da7414349d5d6f345
choi-jihun <102225856+choi-jihun@users.noreply.github.com>
2023-11-11 18:25:38 +0900

    바이러스

---
+++ src/Softeer_6284.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Softeer_6284 {
+    static long MOD = 1000000007;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long K = Long.parseLong(st.nextToken());
+        long P = Long.parseLong(st.nextToken());
+        long N = Long.parseLong(st.nextToken());
+
+        long ans = recursive(P, N);
+
+        System.out.println((ans * K) % MOD);
+    }
+
+    static long recursive(long P, long time) {
+        if (time == 0)
+            return 1;
+
+        if (time == 1)
+            return P % MOD;
+
+        long temp = recursive(P, time / 2) % MOD;
+        temp = (temp * temp) % MOD;
+
+        if (time % 2 == 1) {
+            return (temp * (P % MOD)) % MOD;
+        } else
+            return temp;
+    }
+}
